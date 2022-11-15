4cc60d255636401e55be3c6e0c33449cbae7dcaf
Natalia Rodriguez <86689416+Naty1988@users.noreply.github.com>
2022-11-15 14:55:41 -0300

    Add files via upload

---
+++ api/models/Product.js
@@ -0,0 +1,17 @@
+/**
+ * Product.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: { type: "String", required: true },
+    price: { type: "Number", required: true },
+    stock: { type: "Number", required: true },
+  },
+
+};
+
